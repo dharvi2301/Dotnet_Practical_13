7778ecd9b6303fe9437e05e4f0061ea4251f7981
dharvi-kaneriya <dharvikaneriya@gmail.com>
2025-04-09 15:03:10 +0530

    feature:Implemented the Practical 13 Test 1.

---
+++ Practical_13/Practical_13/Data/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Practical_13.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Practical_13.Data
+{
+	public class AppDbContext:DbContext
+	{
+        public AppDbContext() : base("name=Employee_Db_Practical_13")
+        {
+
+        }
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
